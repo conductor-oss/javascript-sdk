53b905a50ad9a1f57050181a31e9d1285ae86a3f
Dmitry Borisov <dmitry.borisov@siliconmint.com>
2025-09-25 20:47:18 +0300

    retry fetching https://google.com

--- integration-tests/common/LoadTest.test.ts
+++ integration-tests/common/LoadTest.test.ts
@@ -13,33 +13,33 @@ describe("Load Test for ECONNRESET", () => {
   jest.setTimeout(TEST_TIMEOUT);
 
   test(`should handle ${CONCURRENT_REQUESTS} staggered GET requests (1 every 0ms) without ECONNRESET`, async () => {
-    const client = await orkesConductorClient();
-    const executor = new WorkflowExecutor(client);
+    // const client = await orkesConductorClient();
+    // const executor = new WorkflowExecutor(client);
 
-    // To ensure we are making valid API calls, we first create a simple
-    // workflow and start one execution. We will then query this execution's status.
-    const workflowName = "load_test_workflow";
-    await executor.registerWorkflow(true, {
-      name: workflowName,
-      version: 1,
-      tasks: [
-        {
-          name: "simple_task",
-          taskReferenceName: "simple_task_ref",
-          type: TaskType.SIMPLE,
-        },
-      ],
-      timeoutSeconds: 0,
-      inputParameters: [],
-    });
-    const executionId = await executor.startWorkflow({
-      name: workflowName,
-      version: 1,
-    });
+    // // To ensure we are making valid API calls, we first create a simple
+    // // workflow and start one execution. We will then query this execution's status.
+    // const workflowName = "load_test_workflow";
+    // await executor.registerWorkflow(true, {
+    //   name: workflowName,
+    //   version: 1,
+    //   tasks: [
+    //     {
+    //       name: "simple_task",
+    //       taskReferenceName: "simple_task_ref",
+    //       type: TaskType.SIMPLE,
+    //     },
+    //   ],
+    //   timeoutSeconds: 0,
+    //   inputParameters: [],
+    // });
+    // const executionId = await executor.startWorkflow({
+    //   name: workflowName,
+    //   version: 1,
+    // });
 
-    console.log(
-      `Starting load test with workflow execution ID: ${executionId}`
-    );
+    // console.log(
+    //   `Starting load test with workflow execution ID: ${executionId}`
+    // );
     console.log(
       `Sending ${CONCURRENT_REQUESTS} staggered requests (1 every 0ms)...`
     );
@@ -48,7 +48,8 @@ describe("Load Test for ECONNRESET", () => {
     const requestPromises: Promise<any>[] = [];
     for (let i = 0; i < CONCURRENT_REQUESTS; i++) {
       // Start the request but don't wait for it to finish here.
-      requestPromises.push(executor.getWorkflow(executionId, false));
+      //requestPromises.push(executor.getWorkflow(executionId, false));
+      requestPromises.push(fetch(`https://google.com`));
 
       if (i < CONCURRENT_REQUESTS - 1) {
         // Wait 100ms before starting the next request.
