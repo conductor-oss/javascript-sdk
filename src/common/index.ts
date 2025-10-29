dfc2937d1b94e7b0404416cd27d23e203dd82998
Dmitry Borisov <dmitry.borisov@siliconmint.com>
2025-10-29 15:45:43 +0300

    Update index.ts

--- src/common/index.ts
+++ src/common/index.ts
@@ -57,6 +57,7 @@ export * from "./deprecated-types";
  * Export types needed for client's return type in case if user is building another lib on top of sdk with declaration files
  * @deprecated
  * to import all the types below manually while using SDK since these types could change in future without backward compatibility
+ * TODO: remove after April 2026 
  */
 export type {
   Auth,
