7e2f5417b25b46ba2f2063c64007b81b09e9455e
Dmitry Borisov <dmitry.borisov@siliconmint.com>
2025-11-19 05:41:29 +0300

    update client exports

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,28 +1,12 @@
-// Client factory
 export * from "./createConductorClient";
 
-// All clients
-export * from "./clients/metadata";
-export * from "./clients/task";
-export * from "./clients/workflow";
-export * from "./clients/event";
-export * from "./clients/scheduler";
-export * from "./clients/template";
-export * from "./clients/human";
-export * from "./clients/service-registry";
-export * from "./clients/execution";
-export * from "./clients/application";
+export * from "./clients";
 
-// Builders
 export * from "./builders";
 
-// Generators
 export * from "./generators";
 
-// Types
 export * from "./types";
 
-// Helpers
 export { DefaultLogger, noopLogger } from "./helpers/logger";
 export type { ConductorLogger } from "./helpers/logger";
-
