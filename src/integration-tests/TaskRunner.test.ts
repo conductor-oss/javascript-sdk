cd1f1a4ff83addb5af6fea2ea4dc101359d54b9d
Andrew Lai <andrew.lai@orkes.io>
2026-03-10 10:31:51 -0700

    Update tests to be v5 only as they are using the v5 endpoint POST /api/tasks/update-v2

--- src/integration-tests/TaskRunner.test.ts
+++ src/integration-tests/TaskRunner.test.ts
@@ -9,6 +9,7 @@ import {
 } from "../sdk";
 import { cleanupWorkflowsAndTasks } from "./utils/cleanup";
 import { waitForWorkflowStatus } from "./utils/waitForWorkflowStatus";
+import { describeForOrkesV5 } from "./utils/customJestDescribe";
 
 describe("TaskRunner", () => {
   const clientPromise = orkesConductorClient();
@@ -26,85 +27,87 @@ describe("TaskRunner", () => {
     );
   });
 
-  test("worker example ", async () => {
-    const client = await clientPromise;
-    const executor = new WorkflowExecutor(client);
-    const taskName = `jsSdkTest-task-manager-int-test-${Date.now()}`;
-    const workflowName = `jsSdkTest-task-manager-int-test-wf-${Date.now()}`;
+  describeForOrkesV5("worker example (requires update-v2)", () => {
+    test("worker example ", async () => {
+      const client = await clientPromise;
+      const executor = new WorkflowExecutor(client);
+      const taskName = `jsSdkTest-task-manager-int-test-${Date.now()}`;
+      const workflowName = `jsSdkTest-task-manager-int-test-wf-${Date.now()}`;
 
-    const taskRunner = new TaskRunner({
-      client: client,
-      worker: {
-        taskDefName: taskName,
-        execute: async () => {
-          return {
-            outputData: {
-              hello: "From your worker",
-            },
-            status: "COMPLETED",
-          };
+      const taskRunner = new TaskRunner({
+        client: client,
+        worker: {
+          taskDefName: taskName,
+          execute: async () => {
+            return {
+              outputData: {
+                hello: "From your worker",
+              },
+              status: "COMPLETED",
+            };
+          },
         },
-      },
-      options: {
-        pollInterval: 1000,
-        domain: undefined,
-        concurrency: 2,
-        workerID: "",
-      },
-    });
-    taskRunner.startPolling();
+        options: {
+          pollInterval: 1000,
+          domain: undefined,
+          concurrency: 2,
+          workerID: "",
+        },
+      });
+      taskRunner.startPolling();
 
-    expect(taskRunner.isPolling).toEqual(true);
+      expect(taskRunner.isPolling).toEqual(true);
 
-    await executor.registerWorkflow(true, {
-      name: workflowName,
-      version: 1,
-      ownerEmail: "developers@orkes.io",
-      tasks: [simpleTask(taskName, taskName, {})],
-      inputParameters: [],
-      outputParameters: {},
-      timeoutSeconds: 0,
-    });
-    workflowsToCleanup.push({ name: workflowName, version: 1 });
-
-    const { workflowId: executionId } = await executor.executeWorkflow(
-      {
+      await executor.registerWorkflow(true, {
         name: workflowName,
         version: 1,
-      },
-      workflowName,
-      1,
-      `${workflowName}-id`
-    );
-    expect(executionId).toBeDefined();
+        ownerEmail: "developers@orkes.io",
+        tasks: [simpleTask(taskName, taskName, {})],
+        inputParameters: [],
+        outputParameters: {},
+        timeoutSeconds: 0,
+      });
+      workflowsToCleanup.push({ name: workflowName, version: 1 });
 
-    taskRunner.updateOptions({ concurrency: 1, pollInterval: 100 });
+      const { workflowId: executionId } = await executor.executeWorkflow(
+        {
+          name: workflowName,
+          version: 1,
+        },
+        workflowName,
+        1,
+        `${workflowName}-id`
+      );
+      expect(executionId).toBeDefined();
 
-    expect(executionId).toBeDefined();
-    if (!executionId) {
-      throw new Error("Execution ID is undefined");
-    }
+      taskRunner.updateOptions({ concurrency: 1, pollInterval: 100 });
 
-    const workflowStatus = await waitForWorkflowStatus(
-      executor,
-      executionId,
-      "COMPLETED"
-    );
+      expect(executionId).toBeDefined();
+      if (!executionId) {
+        throw new Error("Execution ID is undefined");
+      }
 
-    const [firstTask] = workflowStatus.tasks || [];
-    expect(firstTask?.taskType).toEqual(taskName);
-    expect(workflowStatus.status).toEqual("COMPLETED");
+      const workflowStatus = await waitForWorkflowStatus(
+        executor,
+        executionId,
+        "COMPLETED"
+      );
 
-    await taskRunner.stopPolling();
+      const [firstTask] = workflowStatus.tasks || [];
+      expect(firstTask?.taskType).toEqual(taskName);
+      expect(workflowStatus.status).toEqual("COMPLETED");
 
-    expect(taskRunner.isPolling).toEqual(false);
-    const taskDetails = await executor.getTask(firstTask?.taskId || "");
-    expect(taskDetails?.status).toEqual("COMPLETED");
+      await taskRunner.stopPolling();
 
-    const metadataClient = new OrkesClients(client).getMetadataClient();
-    await cleanupWorkflowsAndTasks(metadataClient, {
-      workflows: [{ name: workflowName, version: 1 }],
-      tasks: [taskName],
-    });
-  }, 120000);
+      expect(taskRunner.isPolling).toEqual(false);
+      const taskDetails = await executor.getTask(firstTask?.taskId || "");
+      expect(taskDetails?.status).toEqual("COMPLETED");
+
+      const metadataClient = new OrkesClients(client).getMetadataClient();
+      await cleanupWorkflowsAndTasks(metadataClient, {
+        workflows: [{ name: workflowName, version: 1 }],
+        tasks: [taskName],
+      });
+    }, 120000);
+  });
 });
